ba2751cc1ad183167654c33c4e9ec9d8792aff08
jacobijy <astony.jy@outlook.com>
2018-04-08 15:48:42 +0800

    add blog template

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
+app.engine('html', require('ejs-mate'));
 
 app.use(logger('dev'));
 app.use(express.json());
