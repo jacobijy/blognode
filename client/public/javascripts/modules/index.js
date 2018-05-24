827b8b40f8f0080fe3a22097fca6b423b6f789cc
jacobijy <astony.jy@outlook.com>
2018-05-25 03:23:48 +0800

    checkout deleted files

--- client/public/javascripts/modules/index.js
+++ client/public/javascripts/modules/index.js
@@ -1,9 +1,7 @@
 import * as article from './article';
 import * as auth from './auth';
-import * as signin from './signin';
-import * as signup from './signup';
 import * as images from './images';
 import * as titles from './titles';
 import * as articles from './articles';
 
-export { article, auth, images, titles, articles, signin, signup }
+export { article, auth, images, titles, articles }
